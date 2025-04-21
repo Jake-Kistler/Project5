6951937aa164694f89cb0180623f9925f976d484
Jake <jakistler01@gmail.com>
2025-04-21 10:05:15 -0500

    Added the points of the project. Will be working on a custom binary tree I think

--- CS3113_Project5.cpp
+++ CS3113_Project5.cpp
@@ -1,8 +1,11 @@
+// This project acts us to calculate the sum of an INT array using threads that re in a binary tree 
+// For input we get the height of the tree
+// The size of the array (numbers of input elements)
+// The ints themselve
+// Need to calculate the number of leaf nodes 
+// detemine if we can divide by the tree height
+// IF NOT: Pad 0's to make it so we can do this division
+// with this value we can pass off N sized sub arrays to the threads
+
 #include <iostream>
-
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+#include <pthread.h>
